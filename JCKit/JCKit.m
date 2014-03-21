a1b9a51cabaf4694262e34f43961dd888f414ced
Josh Wingstrom <joshcodes@Joshs-MacBook-Pro-2.local>
2014-03-21 08:45:27 -0500

    Initial Commit

---
+++ JCKit/JCKit.m
@@ -0,0 +1,13 @@
+//
+//  JCKit.m
+//  JCKit
+//
+//  Created by Josh Wingstrom on 3/21/14.
+//  Copyright (c) 2014 JoshCodes. All rights reserved.
+//
+
+#import "JCKit.h"
+
+@implementation JCKit
+
+@end
